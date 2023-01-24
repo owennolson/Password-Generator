c8c89735df1dfeac650f2639deb5dd5fcbf81716
abc <abc@example.com>
2023-01-23 23:18:35 -0500

    retried to make the js

--- script.js
+++ script.js
@@ -1,64 +1,68 @@
 // Assignment Code
+let lower = "";
+let upper = "";
+let numbers = "";
+let specialChar = "";
+let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numberChoice = "1234567890";
+let specialCharacters = "!@#$%^&*-_=+?><~"
 var generateBtn = document.querySelector("#generate");
-
+function generatePassword() {
+    for (var i = 0; i === passwordLength; i++){
+        console.log("a");
+    }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//asks for length of password
-let passwordLenght = prompt("pick a number 6-100");
-//sets the variables to false by default
-let lower = false;
-let upper = false;
-let numbers = false;
-let specialChar = false;
-//sets the available characters
-let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numberChoice = "1234567890";
-let specialCharacters = "!@#$%^&*-_=+?><~"
-
-function lowerCase() {
-    if (confirm("Would you like to use Lower Case letters?")) {
-        lower = true;
-    } else {
-        lower = false;
+generateBtn.addEventListener("click", function(){
+    function passwordLength() {
+        let passwordLength = prompt("pick a number 8-128");
+        if ((passwordLength < 8) && (passwordLength > 128)) {
+            alert("value out of range, please try again")
+            passwordLength();
+        }
     }
-}
-function upperCase() {
-    if (confirm("Would you like to use Upper Case letters?")) {
-        upper = true;
-    } else {
-        upper = false;
+    function lowerCase() {
+        if (confirm("Would you like to use Lower Case letters?")) {
+            lower = true;
+        } else {
+            lower = false;
+        }
     }
-}
-function numerics() {
-    if (confirm("Would you like to use Numbers?")) {
-        numbers = true;
-    } else {
-        numbers = false;
+    function upperCase() {
+        if (confirm("Would you like to use Upper Case letters?")) {
+            upper = true;
+        } else {
+            upper = false;
+        }
     }
-}
-function specialCharacter() {
-    if (confirm("Would you like to use Special Characters?")) {
-        specialChar = true;
-    } else {
-        specialChar = false;
+    function numerics() {
+        if (confirm("Would you like to use Numbers?")) {
+            numbers = true;
+        } else {
+            numbers = false;
+        }
     }
-}
-if ((passwordLenght > 8) && (passwordLenght < 128)) {
+    function specialCharacter() {
+        if (confirm("Would you like to use Special Characters?")) {
+            specialChar = true;
+        } else {
+            specialChar = false;
+        }
+    }  
+    passwordLength();
     lowerCase();
     upperCase();
     numerics();
     specialCharacter();
-} else {
-    alert ("value did not reach range")
-}
-
-// make functions to check for each constraint. make a big if at the end that if true then they all go in to the pass word
+})
