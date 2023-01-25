bbb5e07151531de61ab9b1e9ffdbfa6ada96717d
abc <abc@example.com>
2023-01-24 21:23:11 -0500

    Finally got the js to work thanks to the tutors help

--- script.js
+++ script.js
@@ -1,68 +1,58 @@
+var passwordLength = 0;
+var choices = [];
+var lowerChoice = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperChoice = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numChoice = ['0','1','2','3','4','5','6','7','8','9'];
+var specialChoice = ['!','@','#','~','$','%','^','&','*','?','>','<'];
+
 // Assignment Code
-let lower = "";
-let upper = "";
-let numbers = "";
-let specialChar = "";
-let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numberChoice = "1234567890";
-let specialCharacters = "!@#$%^&*-_=+?><~"
 var generateBtn = document.querySelector("#generate");
-function generatePassword() {
-    for (var i = 0; i === passwordLength; i++){
-        console.log("a");
-    }
-}
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", function(){
-    function passwordLength() {
-        let passwordLength = prompt("pick a number 8-128");
-        if ((passwordLength < 8) && (passwordLength > 128)) {
-            alert("value out of range, please try again")
-            passwordLength();
-        }
+
+// Write password to the #password input
+function writePassword() {
+  var chosenChar = userInput();
+  var passwordText = document.querySelector("#password");
+
+  if (chosenChar) {
+    var password = generatePassword();
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
+  }
+
+}
+
+function generatePassword() {
+    var newPassword = "";
+    for (var i =0; i < passwordLength; i++) {
+        var randomChar = Math.floor(Math.random() * choices.length);
+        newPassword = newPassword + choices[randomChar];
     }
-    function lowerCase() {
-        if (confirm("Would you like to use Lower Case letters?")) {
-            lower = true;
-        } else {
-            lower = false;
-        }
+    return newPassword;
+}
+
+function userInput() {
+    choices = [];
+    passwordLength = parseInt(prompt("Pick a number 8 - 128"));
+    if (passwordLength  < 8 || passwordLength > 128) {
+        alert("Number not in range. Please try again.");
+        return false;
     }
-    function upperCase() {
-        if (confirm("Would you like to use Upper Case letters?")) {
-            upper = true;
-        } else {
-            upper = false;
-        }
+
+    if (confirm("Would you like to use Lowercase Letters")) {
+        choices = choices.concat(lowerChoice);
     }
-    function numerics() {
-        if (confirm("Would you like to use Numbers?")) {
-            numbers = true;
-        } else {
-            numbers = false;
-        }
+    if (confirm("Would you like to use Uppercase Letters")) {
+        choices = choices.concat(upperChoice);
     }
-    function specialCharacter() {
-        if (confirm("Would you like to use Special Characters?")) {
-            specialChar = true;
-        } else {
-            specialChar = false;
-        }
-    }  
-    passwordLength();
-    lowerCase();
-    upperCase();
-    numerics();
-    specialCharacter();
-})
+    if (confirm("Would you like to use Numbers")) {
+        choices = choices.concat(numChoice);
+    }
+    if (confirm("Would you like to use Special Characters")) {
+        choices = choices.concat(specialChoice);
+    }
+    return true;
+}
